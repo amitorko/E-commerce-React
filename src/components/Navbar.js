4214582e9b19dc759bdb5c825a7799e6fa725cab
amitorko <amitorkofast@gmail.com>
2021-10-08 23:09:51 -0400

    Search icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,50 @@
+import { Search } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-`
-const Wrapper= styled.div`
-    padding 10px 20px;
-    display flex;
-`
-const Left= styled.div``
-const Center= styled.div``
-const Right= styled.div``
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+font-size: 14px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
 
 export default function Navbar() {
     return (
         <Container>
             <Wrapper>
-                <Left>Left</Left>
+                <Left>
+                  <Language>Eng</Language>
+                  <SearchContainer>
+                    input
+                    <Search></Search>
+                  </SearchContainer>
+                </Left>
                 <Center>Center</Center>
                 <Right>Right</Right>
             </Wrapper>
