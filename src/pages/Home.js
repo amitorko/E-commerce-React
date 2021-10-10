64270628ee8cd9282897a879c57dd75d09993096
amitorko <amitorkofast@gmail.com>
2021-10-10 00:33:00 -0400

    Announcement deals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Announcement } from '../components/Announcement'
 import Navbar from '../components/Navbar'
 
 export default function home() {
     return (
         <div className= "container" >
+            <Announcement/>
             <Navbar/>
         </div>
     )
